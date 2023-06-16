0d0641027c0beb5d66d601cb640642535db4d0c7
Orelba <orel450@gmail.com>
2023-06-16 15:49:31 +0300

    Add mock for state structure

---
+++ src/PersonInfo.js
@@ -0,0 +1,130 @@
+export const personInfo = {
+  name: 'Michael Andrews',
+  role: 'Creative Director',
+  avatarUrl: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  profileDescription: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero consequatur fugit optio veniam alias excepturi quidem eaque quia reiciendis? Inventore quibusdam repellendus, ab repudiandae fugit optio maxime voluptate quidem explicabo.',
+  workExperience: [
+    {
+      role: 'Senior Manager',
+      timePeriod: 'JAN 2016 - DEC 2019',
+      companyName: 'LEO Company',
+      companyLocation: 'Paris - France',
+      description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum vitae natus non ducimus rem laudantium fuga architecto, modi ad provident! Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, totam?'
+    },
+    {
+      role: 'Senior Manager',
+      timePeriod: 'JAN 2016 - DEC 2019',
+      companyName: 'LEO Company',
+      companyLocation: 'Paris - France',
+      description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum vitae natus non ducimus rem laudantium fuga architecto, modi ad provident! Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, totam?'
+    },
+    {
+      role: 'Senior Manager',
+      timePeriod: 'JAN 2016 - DEC 2019',
+      companyName: 'LEO Company',
+      companyLocation: 'Paris - France',
+      description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum vitae natus non ducimus rem laudantium fuga architecto, modi ad provident! Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, totam?'
+    },
+  ],
+  skills: [
+    'Project Management',
+    'Executive Presentation',
+    'Attention to Detail',
+    'Collaboration',
+    'Problem Solving',
+    'Negotiation'
+  ],
+  contact: {
+    addressFirstLine: '#247 Dream Valley',
+    addressSecondLine: '7th Avenue, NY - 10025',
+    phoneNumber: '+1 124 568 982',
+    email: 'info@yourmail.com'
+  },
+  education: [
+    {
+      title: "Master's in MBA",
+      timePeriod: 'JAN 2015 - DEC 2018',
+      institution: 'University of New York',
+      country: 'USA',
+    },
+    {
+      title: "Bachelor's in BBA",
+      timePeriod: 'JAN 2015 - DEC 2018',
+      institution: 'University of Washington',
+      country: 'USA',
+    },
+  ],
+  references: [
+    {
+      name: 'Mr. James Collenwie',
+      role: 'Chief Enginner',
+      company: 'MIN Technologies',
+      email: 'somename@mail.com',
+      phoneNumber: '+123 4567 8901'
+    },
+    // {
+    //   name: 'Mr. James Richards',
+    //   role: 'Chief Enginner',
+    //   company: 'Apple',
+    //   email: 'somename@mail.com',
+    //   phoneNumber: '+123 4567 8901'
+    // }
+  ]
+}
+
+// const personTemplate = {
+//   name: '',
+//   role: '',
+//   avatarUrl: '',
+//   profileDescription: '',
+//   workExperience: [
+//     {
+//       role: '',
+//       timePeriod: '',
+//       companyName: '',
+//       companyLocation: '',
+//       description: ''
+//     },
+//   ],
+//   skills: [
+//     '',
+//   ],
+//   contact: {
+//     address: '',
+//     phoneNumber: '',
+//     email: ''
+//   },
+//   education: [
+//     {
+//       title: '',
+//       timePeriod: '',
+//       institution: '',
+//       country: '',
+//     },
+//   ],
+//   references: [
+//     {
+//       name: '',
+//       role: '',
+//       company: '',
+//       email: '',
+//       phoneNumber: ''
+//     }
+//   ]
+// }
+
+// const personEmpty = {
+//   name: '',
+//   role: '',
+//   avatarUrl: '',
+//   profileDescription: '',
+//   workExperience: [],
+//   skills: [],
+//   contact: {
+//     address: '',
+//     phoneNumber: '',
+//     email: ''
+//   },
+//   education: [],
+//   references: []
+// }
