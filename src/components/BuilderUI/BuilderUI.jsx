0db24e98390121afe2e9d05c24567cb95d7fa7c7
Orelba <orel450@gmail.com>
2023-07-23 16:06:33 +0300

    Create Builder UI

--- src/components/BuilderUI/BuilderUI.jsx
+++ src/components/BuilderUI/BuilderUI.jsx
@@ -1,9 +1,76 @@
 import '../../styles/builder-ui.css'
+import PersonalInfoForm from './PersonalInfoForm'
+import ProfileForm from './ProfileForm'
+import WorkExperienceForm from './WorkExperienceForm'
+import SkillsForm from './SkillsForm'
+import ContactForm from './ContactForm'
+import EducationForm from './EducationForm'
+import ReferencesForm from './ReferencesForm'
+import ButtonPanel from './ButtonPanel'
 
-export default function BuilderUI() {
+export default function BuilderUI({
+  cv,
+  onChangePersonalInfo,
+  onChangeContact,
+  onChangeSkill,
+  onAddSkill,
+  onDeleteSkill,
+  onChangeEducation,
+  onAddEducation,
+  onDeleteEducation,
+  onChangeWorkExperience,
+  onAddWorkExperience,
+  onDeleteWorkExperience,
+  onChangeReference,
+  onAddReference,
+  onDeleteReference,
+  onExportPDF,
+  onResetForm,
+  onLoadExampleCV
+}) {
   return (
     <div className='builder-ui-container'>
-      BUILDERUI
+      <PersonalInfoForm
+        cv={cv}
+        onChange={onChangePersonalInfo}
+      />
+      <ProfileForm
+        cv={cv}
+        onChange={onChangePersonalInfo}
+      />
+      <WorkExperienceForm 
+        cv={cv}
+        onChange={onChangeWorkExperience}
+        onAdd={onAddWorkExperience}
+        onDelete={onDeleteWorkExperience}
+      />
+      <SkillsForm
+        cv={cv}
+        onChange={onChangeSkill}
+        onAdd={onAddSkill}
+        onDelete={onDeleteSkill}
+      />
+      <ContactForm
+        cv={cv}
+        onChange={onChangeContact}
+      />
+      <EducationForm 
+        cv={cv}
+        onChange={onChangeEducation}
+        onAdd={onAddEducation}
+        onDelete={onDeleteEducation}
+      />
+      <ReferencesForm 
+        cv={cv}
+        onChange={onChangeReference}
+        onAdd={onAddReference}
+        onDelete={onDeleteReference}
+      />
+      <ButtonPanel 
+        onExportPDF={onExportPDF}
+        onReset={onResetForm}
+        onLoadExample={onLoadExampleCV}
+      />
     </div>
   )
 }
